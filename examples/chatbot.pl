f0c498334829f1ae66e93580c2178b3e2c500aab
thomw201 <thomwouda@gmail.com>
2016-12-16 16:33:42 +0100

    more patterns & random, start with "start."

--- examples/chatbot.pl
+++ examples/chatbot.pl
@@ -7,6 +7,11 @@
 
 :- consult([alice]).
 
+start :-
+    write('Hello, I am a gamebot.'), nl,
+    write('Ask me any game-related question!'), nl,
+loop.
+
 %give description of game
 category([
 	pattern([star(_),tell,me,about,star(Game),star(_)]),
@@ -19,6 +24,8 @@ category([
     pattern([star(_),information,star(_)]),
     template(['Do',you,want,information,about,a,game,'?'])
 ]).
+
+
 
 %user said yes, ask the game name
 category([
@@ -56,13 +63,6 @@ category([
     template([think((atomic_list_concat(A,' ',ConsoleName),getdescriptionofconsole(ConsoleName, Description))), Description])
 ]).
 
-%user said no
-category([
-    pattern([no]),
-    that(['Which',console,do,you,want,to,know,more,about,'?']),
-    template(['Ask',me,anytime,'!'])
-]).
-
 %give developer of game
 category([
 	pattern([star(_),developer,of,star(Game),'?']),
@@ -93,6 +93,12 @@ category([
 	template([think((atomic_list_concat(Game,' ',NewGame),getreleasedate(NewGame, Releasedate))),Game, was, released, on, Releasedate,'.'])
 ]).
 
+%give release date of game
+category([
+	pattern([star(_),was,star(Game), released,'?']),
+	template([think((atomic_list_concat(Game,' ',NewGame),getreleasedate(NewGame, Releasedate))),Game, was, released, on, Releasedate,'.'])
+]).
+
 %get game's rating
 category([
 	pattern([star(_), rating, of,star(Game),'?']),
@@ -101,8 +107,8 @@ category([
 
 %get favourite x game (example: fav mario game)
 category([
-	pattern([what, is, your, favourite, star(Game), game,'?']),
-	template([think((atomic_list_concat(Game,' ',NewGame),getfavouritegame(NewGame, Favouritegame))),'My', favourite, Game, game, is, Favouritegame, '!']) %highest rated game with mario in the name
+	pattern([what, is, the, highest, rated, star(Game), game,'?']),
+	template([think((atomic_list_concat(Game,' ',NewGame),getfavouritegame(NewGame, Favouritegame))), 'The', highest, rated, game, is, Favouritegame,'!']) %highest rated game with mario in the name
 ]).
 
 %show picture of game
@@ -113,7 +119,7 @@ category([
 
 %show a video of game
 category([
-	pattern([can,you,show,a,video, of,star(Game),'?']),
+	pattern([star(_),video, of,star(Game),'?']),
 	template(['Here', is, a, video, of, Game,think((atomic_list_concat(Game,' ',NewGame),showvideoofgame(NewGame)))])
 ]).
 
@@ -144,16 +150,95 @@ category([
     template(['What',game,video,do,you,want,me,to,play,for,you,'?'])
 ]).
 
-
+%user said the name of the game/video he wants to see
 category([
     pattern([star(A)]),
     that(['What',game,video,do,you,want,me,to,play,for,you,'?']),
     template(['Here', is, a, video, of, A,think((atomic_list_concat(A,' ',NewGame),showvideoofgame(NewGame)))])
 ]).
 
+%user wants to know the developer of a game
+category([
+    pattern([yes]),
+    that(['Would',you,like,to,know,the,developer,of,any,game,'?']),
+    template(['Okay,',tell,me,the,title,of,the,game,'!'])
+]).
+
+%user wants to know the developer of a game
+category([
+    pattern([star(A)]),
+    that(['Okay,',tell,me,the,title,of,the,game,'!']),
+    template([think((atomic_list_concat(A,' ',NewGame),getdeveloperofgame(NewGame, Developer))),A, was, developed, by, Developer,'.'])
+]).
+
+%user wants to know the rating of a game
+category([
+    pattern([yes]),
+    that(['Would',you,like,to,know,the,rating,of,a,game,'?']),
+    template(['Okay,',what,is,the,title,of,the,game,'?'])
+]).
+
+%user wants to know the rating of a game
+category([
+    pattern([star(Game)]),
+    that(['Okay,',what,is,the,title,of,the,game,'?']),
+	template([think((atomic_list_concat(Game,' ',NewGame),getratingofgame(NewGame, Rating))),'The', rating, of, Game, is, Rating])
+]).
+
+%user wants to know the publisher of a game
+category([
+    pattern([yes]),
+    that(['Would',you,like,to,know,the,publisher,of,a,game,'?']),
+    template(['So,',tell,me,the,title,of,the,game,'!'])
+]).
+
+%user wants to know the publisher of a game
+category([
+    pattern([star(Game)]),
+    that(['So,',tell,me,the,title,of,the,game,'!']),
+	template([think((atomic_list_concat(Game,' ',NewGame),getpublisher(NewGame, Publisher))),Game, was, published, by, Publisher])
+]).
+
 category([
 	pattern([quit]),
 	template(['Goodbye!', think(break)])
+]).
+
+%user said no. Offer more help about any question!
+category([
+    pattern([no, star(_)]),
+    %template(['Okay!',if,you,have,any,game,related,'questions,',just,ask,'!'])
+		template([random([
+		['Okay!','If',you,have,any,game,related,'questions,',just,ask,'!'],
+		['Please',ask,me,any,game,related,question,'!'],
+		[':(',ask,me,something,about,'ANY',game,'!'],
+		['Anything',else,'?'],
+		['Ok','!',feel,free,to,ask,me,more,game,questions,'.']])
+	])
+]).
+
+%user thanked you for something, be nice!
+category([
+    pattern([thanks,star(_)]),
+		template([random([
+		['Happy',to,help,'!'],
+		['My',pleasure,'!'],
+		['No',problem,'!'],
+		['Anytime','!'],
+		['Can','I',help,you,with,anything,else,'?']])
+	])
+]).
+
+%user thanked you for something, be nice!
+category([
+    pattern([thank,you,star(_)]),
+		template([random([
+		['Happy',to,help,'!'],
+		['My',pleasure,'!'],
+		['No',problem,'!'],
+		['Anytime','!'],
+		['Can','I',help,you,with,anything,else,'?']])
+	])
 ]).
 
 category([
@@ -163,7 +248,11 @@ category([
 		['Do',you,want,information,about,a,console,'?'],
 		['What',is,your,favourite,video,game,'?'],
 		['Do',you,want,to,see,a,video,'?'],
-		['Do',you,like,mario,'?']])
+		['Would',you,like,to,know,the,developer,of,any,game,'?'],
+		['Would',you,like,to,know,the,rating,of,a,game,'?'],
+		['Would',you,like,to,know,the,publisher,of,a,game,'?'],
+		['Do',you,want,to,know,the,release,date,of,a,game,'?'],
+		['Do',you,like,'Mario','?']])
 	])
 ]).
 
